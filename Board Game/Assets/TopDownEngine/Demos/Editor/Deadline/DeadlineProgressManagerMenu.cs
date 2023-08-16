c1411a24a54e5a2c8e4c9c12826a3d98de717e0e
Random Game Dev <sebthproductivity@gmail.com>
2023-08-16 19:44:23 -0400

    IMPORTED TopDown Engine

---
+++ Board Game/Assets/TopDownEngine/Demos/Editor/Deadline/DeadlineProgressManagerMenu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace MoreMountains.TopDownEngine
+{	
+	public static class DeadlineProgressManagerMenu 
+	{
+		[MenuItem("Tools/More Mountains/Reset all Deadline progress",false,21)]
+		/// <summary>
+		/// Adds a menu item to reset all progress in the deadline demo
+		/// </summary>
+		private static void ResetProgress()
+		{
+			DeadlineProgressManager.Instance.ResetProgress ();
+		}
+	}
+}
