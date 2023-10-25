1717177237231c0bbca8c6f22316f360acd83823
Random Game Dev <sebthproductivity@gmail.com>
2023-10-24 22:06:51 -0400

    Custom Run feature doesn't always work

--- Board Game/Assets/0_Game/Custom Scripts/SetPlayerStat.cs
+++ Board Game/Assets/0_Game/Custom Scripts/SetPlayerStat.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using MoreMountains.TopDownEngine;
+using MoreMountains.Tools;
 
 public class SetPlayerStat : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class SetPlayerStat : MonoBehaviour
     [SerializeField] private Character m_character;
     [SerializeField] private Health m_health;
     [SerializeField] private HealthAutoRefill m_healthAutoRefill;
+    [SerializeField] private MMHealthBar m_healthBar;
     [SerializeField] private CharacterMovement m_movement;
     [SerializeField] private CharacterDamageDash3D m_damageDash;
 
@@ -20,6 +22,7 @@ public class SetPlayerStat : MonoBehaviour
         m_movement = m_player.GetComponent<CharacterMovement>();
         m_health = m_player.GetComponent<Health>();
         m_healthAutoRefill = m_player.GetComponent<HealthAutoRefill>();
+        m_healthBar = m_player.GetComponent<MMHealthBar>();
         m_damageDash = m_player.GetComponent<CharacterDamageDash3D>();
     }
 
@@ -34,6 +37,17 @@ public class SetPlayerStat : MonoBehaviour
     public void SetHealthFromPlayerPrefs()
     {
         int health = PlayerPrefs.GetInt("MaxHealth");
+        float difference = health - 300.0f;
+        float Bar_x_size;
+        if (difference > 0)
+        {
+            Bar_x_size = Mathf.Lerp(1.5f, 2.5f, difference / 700.0f);
+        }
+        else
+        {
+            Bar_x_size = Mathf.Lerp(0.5f, 1.5f, health / 300.0f);
+        }
+        m_healthBar.Size.x = Bar_x_size;
         m_health.MaximumHealth = health;
         m_health.CurrentHealth = health;
     }
